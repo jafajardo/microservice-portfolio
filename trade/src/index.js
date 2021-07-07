2ccee846bbe8f4393589175aeec85332cd397c00
Joseph <jafajardo@gmail.com>
2021-07-07 14:05:56 +1000

    update starting message

--- trade/src/index.js
+++ trade/src/index.js
@@ -150,7 +150,7 @@ const start = async () => {
   }
 
   const PORT = process.env.PORT || 8000;
-  app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
+  app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
 };
 
 start();
